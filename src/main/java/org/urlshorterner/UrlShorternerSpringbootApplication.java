7f4692a5f0a1b5b93ae06cdd72d0cb417c2a1041
Aditya Shendkar <aditya.shendkar03@gmail.com>
2026-05-06 13:10:00 +0530

    Initial files

---
+++ src/main/java/org/urlshorterner/UrlShorternerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.urlshorterner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorternerSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShorternerSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/urlshorterner/UrlShorternerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.urlshorterner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShorternerSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
